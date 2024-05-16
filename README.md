82858fab91bb82ea0d3fe056b1928d581f54076e
Jooho Lee <jlee30112562@gmail.com>
2024-05-15 19:03:44 -0700

    added website programs

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # myWebsite
 
 Github Repository for Jooho Lee's website
+
+email: jlee30112562@gmail.com
+Discord: jooho1
---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Programmer", "UCSC Computer Science B.S. Student", "Game Addict"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
